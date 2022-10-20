d040e1d043d6e3faa080d57571c1013adfe94415
eh21 <eh21@ic.ac.uk>
2022-10-20 16:22:59 +0800

    feat: 11

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def run_query(query):
 
 sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
+# st.write(rows)
 
 # Print results.
 for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+    st.write(row.Stations)
